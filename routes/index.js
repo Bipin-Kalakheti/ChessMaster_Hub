e546fea19f082de18a682afbc3865ad7e135e3ac
Bipin-Kalakheti <100770518+Bipin-Kalakheti@users.noreply.github.com>
2023-12-07 10:36:32 -0500

    Update Index.js
    Removed Commented parts

--- routes/index.js
+++ routes/index.js
@@ -14,22 +14,7 @@ router.get('/home', function(req, res, next) {
   res.render('index', { title: 'Landing' });
 });
 
-/* GET Database Page page. */
 
-// router.get('/tournaments', function(req, res, next) {
-//   res.render('tournaments', { title: 'Tournaments' });
-// });
-// router.get('/tournaments', async (req, res) => {
-//   console.log('Tournament.js');
-//   try {
-//       const items = await Tournament.find();
-//       console.log(items);
-//       res.render('Tournament.ejs', { items, title: "Tournaments"}); // Render the EJS view and pass items as a local variable
-//   } catch (error) {
-//       console.error(error);
-//       res.status(500).json({ error: 'Internal Server Error' });
-//   }
-// });
 
 
 
